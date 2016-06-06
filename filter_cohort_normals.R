0988d0c32101095f3e31256bbd833be4a1c8cf9c
Nicholas D. Socci <soccin@mskcc.org>
2016-06-06 18:31:13 -0400

    Updated filter_cohort_normal to accept file of normal samples Added -N (--normalSamplesFile) option which allows on to specify explicitly which samples in the cohort are the normals rather than trying to compute them from a regular expression

--- filter_cohort_normals.R
+++ filter_cohort_normals.R
@@ -6,23 +6,16 @@
 # annotate somatic variant calls accordingly
 ##########################################################################################
 
-annotate_maf <- function(maf, fillout,
-                         alt.reads = 3,
-                         normal.regex = '') {
+annotate_maf <- function(maf, fillout, normal.samples, alt.reads = 3) {
 
   # select normal samples
-  if (normal.regex!="") {
-    cat("using user supplied regex",normal.regex,"\n")
-    fillout <- fillout[Tumor_Sample_Barcode %like% normal.regex]
-  } else {
-    fillout <- fillout[Tumor_Sample_Barcode %like% "*N$"]
-  }
+  fillout <- fillout[Tumor_Sample_Barcode %in% normal.samples]
 
   # identify loci with 3+ alternate reads in any normal sample
   fillout <- fillout[t_alt_count >= alt.reads]
 
   if(nrow(fillout)==0){
-    cat("**WARNING** cohort normal fillout empty. Check normal.regex=r/",normal.regex,"/\n")
+    cat("**WARNING** cohort normal fillout empty. Check normal samples or normal.regex=r/",normal.regex,"/\n")
   }
 
   # Add TAG to MAF
@@ -63,6 +56,7 @@ if( ! interactive() ) {
   parser$add_argument('-n', '--reads', type='character', default=3, help='Alternate read threshold')
   parser$add_argument('-o', '--outfile', type='character', help='Output file', default = 'stdout')
   parser$add_argument('-r', '--regex', type='character', help='RegEx for normals', default = '')
+  parser$add_argument('-N', '--normalSamplesFile', type='character', help='File with list of normal samples', default='')
   args=parser$parse_args()
 
   if (args$maf == 'stdin') { maf = suppressWarnings(fread('cat /dev/stdin', showProgress = F))
@@ -71,8 +65,24 @@ if( ! interactive() ) {
   alt.reads <- args$reads
   outfile <- args$outfile
   normal.regex <- args$regex
+  normalSamplesFile <- args$normalSamplesFile
 
-  maf.out <- annotate_maf(maf, fillout, normal.regex=normal.regex)
+  # get normal samples
+
+  if(normalSamplesFile!="") {
+
+    normal.samples=scan(normalSamplesFile,"")
+
+  } else {
+
+    fillout.samples=unique(fillout$Tumor_Sample_Barcode)
+    if (normal.regex=="") {
+      normal.regex="*N$"
+    }
+    normal.samples=fillout.samples[grepl(normal.regex,fillout.samples)]
+  }
+
+  maf.out <- annotate_maf(maf, fillout, normal.samples)
   if (outfile == 'stdout') { write.table(maf.out, stdout(), sep = "\t", col.names = T, row.names = F, quote = F, na="")
   } else { write.table(maf.out, outfile, sep = "\t", col.names = T, row.names = F, quote = F, na="") }
 }
