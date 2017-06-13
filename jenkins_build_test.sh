48dbdeb9808b5c0dee5e7f30dfbc7646f0db446b
Chris Harris <ccharris@gmail.com>
2017-06-13 13:51:59 -0400

    fix tests to run in our env, in temp dir

--- jenkins_build_test.sh
+++ jenkins_build_test.sh
@@ -1,5 +1,5 @@
 PYTHON_HOME=/opt/common/CentOS_6-dev/python/python-2.7.10/bin/
-export PATH=$PATH:/opt/common/CentOS_6-dev/bin/current/
+export PATH=//opt/common/CentOS_6-dev/python/python-2.7.10/bin/:/opt/common/CentOS_6-dev/R/R-3.2.2/bin/:$PATH:/opt/common/CentOS_6-dev/bin/current/
 
     
     # Create virtualenv and install necessary packages
